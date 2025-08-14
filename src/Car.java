4da807e4c810a47a5a5877c36197445bca78ab9d
fahadLine <152792332+fahadLine@users.noreply.github.com>
2025-08-14 16:02:14 +0300

    Create Car.java

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+
+    int id;
+    String name;
+    int price;
+    String type;
+
+    Movie(int id, String name, int year, double rate, String genre){
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.rate = rate;
+        this.genre = genre;
+    }
+    
+}
