92309c05925295f61505a899d06cfaeb337fbfbf
fahadLine <152792332+fahadLine@users.noreply.github.com>
2025-08-14 16:00:18 +0300

    Add files via upload

---
+++ src/Movie.java
@@ -0,0 +1,17 @@
+public class Movie {
+
+    int id;
+    String name;
+    int year;
+    double rate;
+    String genre;
+
+    Movie(int id, String name, int year, double rate, String genre){
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.rate = rate;
+        this.genre = genre;
+    }
+    
+}
